16b2d417e8707fedc4b31162023913e7bb8e9302
Kalpesh <kalpeshfadthare@gmail.com>
2023-02-04 19:00:26 +0530

    infunction request done

--- server.js
+++ server.js
@@ -46,21 +46,25 @@ app.post("/hello",async function(req,res){
 		console.log(d2);
 		};
 	});
+	PPPCAL(d,d2);
 	// var response= await fetch(`https://v6.exchangerate-api.com/v6/8ec6b5690d95492d22fe1b0c/pair/${c1}/${c2}/1`);
-	var response= await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API}/pair/${c1}/${c2}/1`);
+	//var response= await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API}/pair/${c1}/${c2}/1`);
 
 
 	// console.log(`https://v6.exchangerate-api.com/v6/8ec6b5690d95492d22fe1b0c/pair/${c1}/${c2}/1`)
-	var data =await response.json();
-	console.log("Conversion Rate:",data.conversion_result);
-	// console.log("Conversion Rate:");
+	//var data =await response.json();
+	//console.log("Conversion Rate:",data.conversion_result);
+	 console.log("Conversion Rate:");
 	
 	var f=d/d2;
 	// console.log(f);
 	f1=f*req.body.amount;
 	console.log("PPP Conversion:",f1.toFixed(2));
 	// res.send("conversion rate:",data.conversion_result);
-	res.send(`<h>conversion rate:${data.conversion_result}</h><br><h>PPP Conversion:${f1.toFixed(2)}</h>`);
+	//res.send(`<h>conversion rate:${data.conversion_result}</h><br><h>PPP Conversion:${f1.toFixed(2)}</h>`);
+	function PPPCAL(source,target){
+		
+	}
 });
 
 app.listen(PORT,function(){
